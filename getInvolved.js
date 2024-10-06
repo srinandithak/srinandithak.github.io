89df95e76f9da7526a5adeb8ddb8033b290ef2fa
srinandithak <117042311+srinandithak@users.noreply.github.com>
2024-10-06 11:29:23 -0500

    Add files via upload

---
+++ getInvolved.js
@@ -0,0 +1,3 @@
+document.getElementById("add").addEventListener("click", function() {
+    window.open("https://calendar.google.com/calendar/event?action=TEMPLATE&tmeid=NmZwZjkyamFjODl1NGhqaDIyYWxubnU3OW8gc2tpbmZvcnNraW4uc2ZzQG0&tmsrc=skinforskin.sfs%40gmail.com");
+});
